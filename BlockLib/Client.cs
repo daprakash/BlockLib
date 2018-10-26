1292ab38b0257a42c5542688b925e63f33c61b9d
Vlad Perevalov <uperaval@students.kennesaw.edu>
2018-10-26 13:32:34 -0400

    first commit

---
+++ BlockLib/Client.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockLib
+{
+   class Client
+   {
+      public string _url { get; set; }
+      public List<Account> _Accounts { get; set; }
+      private HttpWebRequest _Web { get; set; }
+      public Client(string url)
+      {
+         _url = url;
+         _Web = (HttpWebRequest)WebRequest.Create(_url);
+         _Web.ContentType = "application/json";
+         _Web.Method = "POST";
+         _Accounts = new List<Account>();
+      }
+      public JObject GenAccount(string password)
+      {
+         JObject response = JsonCom.InvokeMethod(_Web, "personal_Account", password);
+         _Accounts.Add(new Account(_url, response.Path));
+         return response;
+      }
+      public JObject GetBalance(Account account)
+      {
+         JObject response = JsonCom.InvokeMethod(_Web, "eth_getBalance");
+         return response;
+      }
+
+      public JObject SendTransaction()
+      {
+         JObject response = jsonMethod.InvokeMethod(_Web, "eth_sendTransaction");
+         return response
+
+      }
+   }
+   public class Account : Client
+   {
+      public string pubKey { get; set; }
+      public string url { get; set; }
+      public Account(string url, string pubKey) : base(url)
+      {
+         this.pubKey = pubKey;
+
+      }
+   }
+}
---
+++ BlockLib/JsonCom.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockLib
+{
+    public class JsonCom
+    {
+      public static JObject InvokeMethod(HttpWebRequest web, string a_sMethod, params object[] a_params)
+      {
+         HttpWebRequest _Web = web;
+
+         JObject joe = new JObject();
+         joe["jsonrpc"] = "2.0";
+         joe["method"] = a_sMethod;
+
+         if (a_params != null)
+         {
+            //if (a_params.Length > 0)
+            //{
+            //   JArray props = new JArray();
+            //   foreach (var p in a_params)
+            //   {
+            //      props.Add(p);
+            //   }
+            //   joe.Add(new JProperty("params", props));
+            //}
+            JArray props = new JArray();
+            foreach (var p in a_params)
+            {
+               props.Add(p);
+            }
+            joe.Add(new JProperty("params", props));
+
+         }
+         joe["id"] = "1";
+
+
+         string s = JsonConvert.SerializeObject(joe);
+         // serialize json for the request
+         byte[] byteArray = Encoding.UTF8.GetBytes(s);
+         _Web.ContentLength = byteArray.Length;
+
+         try
+         {
+            using (Stream dataStream = _Web.GetRequestStream())
+            {
+               dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+         }
+         catch (WebException we)
+         {
+            //inner exception is socket
+            //{"A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond 23.23.246.5:8332"}
+            throw;
+         }
+         WebResponse webResponse = null;
+         try
+         {
+            using (webResponse = _Web.GetResponse())
+            {
+               using (Stream str = webResponse.GetResponseStream())
+               {
+                  using (StreamReader sr = new StreamReader(str))
+                  {
+                     return JsonConvert.DeserializeObject<JObject>(sr.ReadToEnd());
+                  }
+               }
+            }
+         }
+         catch (WebException webex)
+         {
+
+            using (Stream str = webex.Response.GetResponseStream())
+            {
+               using (StreamReader sr = new StreamReader(str))
+               {
+                  JObject tmp = (JObject)sr.ReadToEnd();
+
+                  var tempRet = JsonConvert.DeserializeObject<JObject>(tmp.ToString());
+                  return tempRet;
+               }
+            }
+
+         }
+         catch (Exception)
+         {
+
+            throw;
+         }
+      }
+   }
+}
